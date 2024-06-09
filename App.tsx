a667c2b099361df4f30343240c5f31e89d66b6d6
Muhammad Rizky Ferdiansyah <103484286+rizkyfrdiansyah@users.noreply.github.com>
2024-06-09 16:40:16 +0700

    Height and Width

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Text, View } from "react-native";
 import LotsOfStyles from "./components/styles/styless";
+import FixedDimensionsBasics from "./components/heightWidth/FixedDimensions";
+import FlexDimensionsBasics from "./components/heightWidth/FlexDimensions";
+import PercentageDimensionsBasics from "./components/heightWidth/PercentageDimensions";
 
 function App() {
   return (
     <View>
       <Text>
         <LotsOfStyles/>
+        <FixedDimensionsBasics/>
+        <FlexDimensionsBasics/>
+        <PercentageDimensionsBasics/>
       </Text>
     </View>
   )
